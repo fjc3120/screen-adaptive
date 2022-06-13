65e25fc5addf2ccb419b2ae8ce513987fc4346e3
fjc3120 <287473791@qq.com>
2022-06-13 16:02:21 +0800

    react

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+ReactDOM.render(
+    <>
+        <span className="streamer"></span>
+        <span className="streamer"></span>
+        <span className="streamer"></span>
+        <span className="streamer"></span>
+    </>,
+    document.getElementById('app-screen')
+);
